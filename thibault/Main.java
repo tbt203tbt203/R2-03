522569cb398c8d4c6fb2c7479c4a5a0b9969f0c5
Thibault Frappart <thibault.frappart@etu.univ-littoral.fr>
2026-06-10 10:54:51 +0200

    Ajout Main.java initial

---
+++ thibault/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    // Calcule la moyenne de deux notes
+    public static double moyenne(double note1, double note2) {
+        return 0; // À compléter
+    }
+
+    // Retourne la meilleure note
+    public static double meilleureNote(double note1, double note2) {
+        return 0; // À compléter
+    }
+
+    /*
+     * Affiche les informations du binôme
+     * Attention : chacun ajoute sa ligne
+     */
+    public static void infosBinome() {
+        // System.out.println("Nom Prénom 1");
+        // System.out.println("Nom Prénom 2");
+    }
+
+    public static void main(String[] args) {
+
+        double note1 = 12.5;
+        double note2 = 15.0;
+
+        System.out.println("La moyenne est : " + moyenne(note1, note2));
+        System.out.println("La meilleure note est : " + meilleureNote(note1, note2));
+
+        infosBinome();
+    }
+}
